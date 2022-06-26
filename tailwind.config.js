41130655acb35c20de4ff6aa994ebc16567906e9
Antonio Cardenas <antonioalx66@gmail.com>
2022-06-25 23:37:17 -0400

    graph flows

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {},
   },
+  variants: {
+    extend: {},
+  },
   plugins: [],
-}
+};
